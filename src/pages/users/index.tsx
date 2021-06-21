7e840ac3631b28ff638681dce67ec15f67325c48
Matheus Neves <matheusneves1989@gmail.com>
2021-06-21 20:01:42 -0300

    feat: Listing users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,7 +12,17 @@ export default function UserList() {
   const { data, isLoading, error } = useQuery('users', async () => {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
-    return data;
+
+    const users = data.users.map(user => ({
+      ...user,
+      formattedCreatedAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }))
+
+    return users;
   })
 
   const isWideVersion = useBreakpointValue({
@@ -69,29 +79,33 @@ export default function UserList() {
                     </Tr>
                   </Thead>
                   <Tbody>
-                    <Tr>
-                      <Td px={["4", "4", "6"]}>
-                        <Checkbox colorScheme="pink" />
-                      </Td>
-                      <Td>
-                        <Box>
-                          <Text fontWeight="bold">Matheus Neves</Text>
-                          <Text fontSize="small" color="gray.300">matheusneves1989@gmail.com</Text>
-                        </Box>
-                      </Td>
-                      {isWideVersion && (<Td> 04 de Abril, 2021 </Td>)}
-                      <Td align="right">
-                        <Button
-                          as="a"
-                          size="sm"
-                          fontSize="sm"
-                          colorScheme="purple"
-                          leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                        >
-                          Editar
-                        </Button>
-                      </Td>
-                    </Tr>
+                    {
+                      data.map(user => (
+                        <Tr key={user.id}>
+                          <Td px={["4", "4", "6"]}>
+                            <Checkbox colorScheme="pink" />
+                          </Td>
+                          <Td>
+                            <Box>
+                              <Text fontWeight="bold">{user.name}</Text>
+                              <Text fontSize="small" color="gray.300">{user.email}</Text>
+                            </Box>
+                          </Td>
+                          {isWideVersion && (<Td> {user.formattedCreatedAt} </Td>)}
+                          <Td align="right">
+                            <Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                            >
+                              Editar
+                            </Button>
+                          </Td>
+                        </Tr>
+                      ))
+                    }
 
                   </Tbody>
                 </Table>
